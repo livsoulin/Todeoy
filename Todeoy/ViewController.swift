8b3cf76a3d5d86240ca106146628e667ca32a44a
liv soulin <livsoulin@livsoulins-macbook-pro.local>
2018-06-19 20:18:10 +0700

    Initial Commit

---
+++ Todeoy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todeoy
+//
+//  Created by livsoulin on 6/19/18.
+//  Copyright © 2018 livsoulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
